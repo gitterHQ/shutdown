2c1a368d0b44e569d092da52b355886bf0192934
Andrew Newdigate <andrew@troupe.co>
2014-05-06 15:48:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 shutdown
 ========
 
-Sequenced shutdown events for graceful shutdown in Node
+Sequenced shutdown events for graceful shutdown in Node.
+
+```
+npm install shutdown
+```
+
+Add your shutdown handlers:
+```
+/* Add a mongo shutdown handler at priority 10 */
+shutdown.addHandler('mongo', 10, function(callback) {
+  /* Quit your mongo connections here */
+  callback();
+});
+
+/* Add a mongo shutdown handler at priority 9 */
+shutdown.addHandler('redis', 9, function(callback) {
+  /* Quit your redis connections here */
+  callback();
+});
+```
+
+When you want to shutdown:
+```
+/* Mongo shutdown handlers will be called first, then redis handlers next, etc */
+shutdown.shutdownGracefully();
+
+```
+
+If you want to provide an exit code for the process
+```
+shutdown.shutdownGracefully(1);
+```
+
+
+Licence
+=======
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Troupe Technology Limited.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ tests/shutdown-coordinator-test.js
@@ -0,0 +1,122 @@
+/*jshint node:true, unused: true */
+/*global describe:true, it: true */
+"use strict";
+
+var ShutdownCoordinator = require('../lib/shutdown-coordinator');
+var assert = require('assert');
+
+describe('shutdown-coordinator', function() {
+  it('should perform shutdown stages in reverse order', function(done) {
+    var underTest = new ShutdownCoordinator();
+
+    var lastCalled;
+
+    underTest.addHandler('three', 3, function(callback) {
+      assert.strictEqual(9, lastCalled);
+      lastCalled = 3;
+      setTimeout(callback, 1);
+    });
+
+    underTest.addHandler('nine', 9, function(callback) {
+      assert.strictEqual(10, lastCalled);
+      lastCalled = 9;
+      setTimeout(callback, 1);
+    });
+
+    underTest.addHandler('ten', 10, function(callback) {
+      assert(!lastCalled);
+      lastCalled = 10;
+      setTimeout(callback, 1);
+    });
+
+    underTest.shutdownGracefully(function(err) {
+      if(err) return done(err);
+
+      assert.strictEqual(3, lastCalled);
+      done();
+    });
+
+  });
+
+  it('should deal with stages that error', function(done) {
+    var underTest = new ShutdownCoordinator();
+
+    var lastCalled;
+
+    underTest.addHandler('ten', 10, function(/*callback*/) {
+      assert(!lastCalled);
+      lastCalled = 10;
+      throw new Error();
+    });
+
+    underTest.addHandler('nine', 9, function(callback) {
+      assert.strictEqual(10, lastCalled);
+      lastCalled = 9;
+      callback(new Error());
+    });
+
+    underTest.addHandler('three', 3, function(callback) {
+      assert.strictEqual(9, lastCalled);
+      lastCalled = 3;
+      setTimeout(callback, 1);
+    });
+
+    underTest.shutdownGracefully(function(err) {
+      if(err) return done(err);
+
+      assert.strictEqual(3, lastCalled);
+      done();
+    });
+
+  });
+
+  it('should deal with shutdown timeouts', function(done) {
+    var underTest = new ShutdownCoordinator({ shutdownTimeout: 10 });
+
+    var lastCalled;
+
+    underTest.addHandler('ten', 10, function(callback) {
+      lastCalled = 10;
+      setTimeout(callback, 15);
+    });
+
+    underTest.addHandler('nine', 9, function(callback) {
+      lastCalled = 9;
+      setTimeout(callback, 1);
+    });
+
+    underTest.shutdownGracefully(function(err) {
+      assert(err);
+      assert.strictEqual(10, lastCalled);
+      done();
+    });
+
+  });
+
+
+  it('should deal with stage timeouts', function(done) {
+    var underTest = new ShutdownCoordinator({ stageTimeout: 10 });
+
+    var lastCalled;
+
+    underTest.addHandler('ten', 10, function(callback) {
+      lastCalled = 10;
+      setTimeout(callback, 15);
+    });
+
+    underTest.addHandler('nine', 9, function(callback) {
+      assert.strictEqual(10, lastCalled);
+
+      lastCalled = 9;
+      setTimeout(callback, 1);
+    });
+
+    underTest.shutdownGracefully(function(err) {
+      if(err) return done(err);
+      assert.strictEqual(9, lastCalled);
+      done();
+    });
+
+  });
+
+});
